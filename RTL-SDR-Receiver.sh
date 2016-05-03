1f4a7b7eb03058d8fc567f8ecefe0d7b97b60c50
Beta <moyuejian@outlook.com>
2016-05-04 06:43:37 +0800

    Update RTL-SDR-Receiver.sh

--- RTL-SDR-Receiver.sh
+++ RTL-SDR-Receiver.sh
@@ -7,3 +7,7 @@ git clone https://github.com/ha7ilm/qtcsdr
 cd qtcsdr
 ./rpi-install.sh
 ./rpi-test.sh
+
+#I want to run stl_sdr receiver withou GUI to acclalte
+#https://github.com/ha7ilm/rtl-sdr
+#我想直接用libcsdr直接输出音频，减少资料利用。
