4e11847cff832d666cd0b7123a957be9e31f665a
Beta <moyuejian@outlook.com>
2016-05-03 18:27:32 +0800

    Create RTL-SDR-Receiver.sh
    command to install a RTL-SDR-Receiver

---
+++ RTL-SDR-Receiver.sh
@@ -0,0 +1,9 @@
+#here is command to install qtcsdr in RP
+#qtcsdr
+#qtcsdr makes a ham transceiver out of your Raspberry Pi 2 and RTL-SDR!
+#https://github.com/ha7ilm/qtcsdr
+
+git clone https://github.com/ha7ilm/qtcsdr
+cd qtcsdr
+./rpi-install.sh
+./rpi-test.sh
